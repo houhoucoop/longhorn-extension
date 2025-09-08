32818e32e9ceca226ec30fe4a2434e8ba0ccd536
Yi-Ya Chen <yiya.chen@suse.com>
2025-09-08 15:47:40 +0800

    ci: build extension on push
    Signed-off-by: Yi-Ya Chen <yiya.chen@suse.com>

--- pkg/longhorn/models/longhornpoc/longhorn.io.node.js
+++
@@ -1,8 +0,0 @@
-import SteveModel from '@shell/plugins/steve/steve-class';
-
-export default class NodesModel extends SteveModel {
-  get availableActions() {
-    console.log("🚀🚀🚀🚀🚀 ~ LonghornNodes ~ getavailableActions ~ availableActions:")
-    return []
-  }
-}
