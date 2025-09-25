5e21ef44b05eb2abc4fff7a7195f022b8c3fbb6a
Yi-Ya Chen <yiya.chen@suse.com>
2025-09-25 13:32:56 +0800

    chore: add longhorn alias
    Signed-off-by: Yi-Ya Chen <yiya.chen@suse.com>

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,27 @@
-const config = require('@rancher/shell/vue.config'); // eslint-disable-line @typescript-eslint/no-var-requires
+const path = require("path");
+const config = require("@rancher/shell/vue.config");
 
-module.exports = config(__dirname, {
-  excludes: [],
-  // excludes: ['fleet', 'example']
-});
+module.exports = () => {
+  const vendorConfig = config(__dirname, { excludes: [] });
+
+  if (typeof vendorConfig.chainWebpack === "function") {
+    const originalChainWebpack = vendorConfig.chainWebpack;
+
+    vendorConfig.chainWebpack = (webpackConfig) => {
+      originalChainWebpack(webpackConfig);
+      webpackConfig.resolve.alias.set(
+        "@longhorn",
+        path.resolve(__dirname, "pkg/longhorn")
+      );
+    };
+  } else {
+    vendorConfig.chainWebpack = (webpackConfig) => {
+      webpackConfig.resolve.alias.set(
+        "@longhorn",
+        path.resolve(__dirname, "pkg/longhorn")
+      );
+    };
+  }
+
+  return vendorConfig;
+};
